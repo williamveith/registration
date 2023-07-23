5199a5398fb17a2a577f7211bbe9d3dc7d57d613
William Veith <williamveith@gmail.com>
2023-07-23 03:40:32 -0500

    added jsdoc comments

--- Send Messages.js
+++ Send Messages.js
@@ -1,3 +1,35 @@
+/**
+ * The message configuration object that defines settings for different types of messages.
+ *
+ * @typedef {Object} MessageConfigs
+ *
+ * @property {Object} Lab Access Account Text - Configuration for Lab Access Account Text messages.
+ * @property {string} Lab Access Account Text.displayName - The display name for the Lab Access Account Text message.
+ * @property {string} Lab Access Account Text.sendAs - The sender email address for the Lab Access Account Text message.
+ * @property {string} Lab Access Account Text.sendTo - The recipient phone number for the Lab Access Account Text message.
+ * @property {string} Lab Access Account Text.subject - The subject for the Lab Access Account Text message.
+ * @property {string} Lab Access Account Text.bodyTemplateName - The template name for the body of the Lab Access Account Text message.
+ *
+ * @property {Object} Lab Access Account Email - Configuration for Lab Access Account Email messages.
+ * @property {string} Lab Access Account Email.displayName - The display name for the Lab Access Account Email message.
+ * @property {string} Lab Access Account Email.sendAs - The sender email address for the Lab Access Account Email message.
+ * @property {string} Lab Access Account Email.subject - The subject for the Lab Access Account Email message.
+ * @property {string} Lab Access Account Email.bodyTemplateName - The template name for the body of the Lab Access Account Email message.
+ *
+ * @property {Object} Basket Assignment Email - Configuration for Basket Assignment Email messages.
+ * @property {string} Basket Assignment Email.displayName - The display name for the Basket Assignment Email message.
+ * @property {string} Basket Assignment Email.sendAs - The sender email address for the Basket Assignment Email message.
+ * @property {string} Basket Assignment Email.bodyTemplateName - The template name for the body of the Basket Assignment Email message.
+ *
+ * @property {function} getConfigs - A function to get the message configuration by providing a configuration key.
+ * @param {string} configKey - The configuration key to retrieve the specific message configuration.
+ * @returns {Object} The configuration object for the given configuration key.
+ *
+ * @property {function} getBody - A function to generate the message body by providing the configuration and dynamic data.
+ * @param {Object} configs - The message configuration object (e.g., Lab Access Account Text or Lab Access Account Email).
+ * @param {Object} dynamicData - The dynamic data to be used for populating the message template.
+ * @returns {string} The message body content generated from the provided template and dynamic data.
+ */
 const messageConfigs = {
   "Lab Access Account Text": {
     displayName: "New User Setup",
@@ -27,6 +59,22 @@ const messageConfigs = {
   }
 }
 
+/**
+ * Sends a text message to a recipient based on the provided user information object.
+ *
+ * @param {Object} userInfoObj - The user information object containing the necessary data for sending the text message.
+ * @param {string} userInfoObj.eid - The lowercase Employee ID (EID) of the user.
+ * @param {string} userInfoObj.firstName - The first name of the user.
+ * @param {string} userInfoObj.lastName - The last name of the user.
+ * @param {string} userInfoObj.activation - The activation date for the user's account.
+ *
+ * @example
+ * // Assuming the user information object is already defined as "userInfo".
+ * // Send a text message based on the provided user information.
+ * sendText(userInfo);
+ *
+ * // The text message will be sent to the recipient specified in the configuration.
+ */
 function sendText(userInfoObj) {
   const configs = messageConfigs.getConfigs("Lab Access Account Text");
   GmailApp.sendEmail(configs.sendTo, configs.subject, "", {
@@ -36,6 +84,23 @@ function sendText(userInfoObj) {
   });
 }
 
+/**
+ * Sends a confirmation email to the user's email address based on the provided user information object.
+ *
+ * @param {Object} userInfoObj - The user information object containing the necessary data for sending the confirmation email.
+ * @param {string} userInfoObj.eid - The lowercase Employee ID (EID) of the user.
+ * @param {string} userInfoObj.firstName - The first name of the user.
+ * @param {string} userInfoObj.lastName - The last name of the user.
+ * @param {string} userInfoObj.email - The email address of the user to which the confirmation email will be sent.
+ * @param {string} userInfoObj.activation - The activation date for the user's account.
+ *
+ * @example
+ * // Assuming the user information object is already defined as "userInfo".
+ * // Send a confirmation email to the user's email address.
+ * sendConfirmationEmail(userInfo);
+ *
+ * // The confirmation email will be sent to the user's email address specified in the configuration.
+ */
 function sendConfirmationEmail(userInfoObj) {
   const configs = messageConfigs.getConfigs("Lab Access Account Email");
   GmailApp.sendEmail(userInfoObj.email, configs.subject, "", {
@@ -45,6 +110,27 @@ function sendConfirmationEmail(userInfoObj) {
   });
 }
 
+/**
+ * Sends a basket assignment email to the user's email address based on the provided basket object.
+ *
+ * @param {Object} basketObj - The basket object containing the necessary data for sending the assignment email.
+ * @param {string} basketObj.basket - The name or ID of the basket.
+ * @param {string} basketObj.status - The status of the basket assignment ("Assign" or "Return").
+ * @param {string} basketObj.firstName - The first name of the user to whom the basket is assigned/returned.
+ * @param {string} basketObj.lastName - The last name of the user to whom the basket is assigned/returned.
+ * @param {string} basketObj.email - The email address of the user to whom the basket is assigned/returned.
+ *
+ * @param {GoogleAppsScript.Base.Blob|undefined} [qrCodeFile=undefined] - The QR code file (Blob) to attach to the email.
+ * If not provided, no attachment will be added.
+ *
+ * @example
+ * // Assuming the basket object and QR code file (Blob) are already defined as "basket" and "qrCodeFile".
+ * // Send a basket assignment email to the user's email address.
+ * sendBasketAssignmentEmail(basket, qrCodeFile);
+ *
+ * // The basket assignment email will be sent to the user's email address specified in the configuration.
+ * // If a QR code file (Blob) is provided, it will be attached to the email.
+ */
 function sendBasketAssignmentEmail(basketObj, qrCodeFile = undefined) {
   const configs = messageConfigs.getConfigs("Basket Assignment Email");
   const dynamicData = {
--- onFormSubmit.js
+++ onFormSubmit.js
@@ -1,3 +1,14 @@
+/**
+ * Throws an error indicating that the current row has already been processed.
+ * This function is used to prevent duplicate processing of rows in the sheet.
+ *
+ * @param {any[]} values - The values of the row that has already been processed.
+ * @throws {string} - A JSON stringified error object with a reason and rowInfo properties.
+ * @example
+ * // Example usage:
+ * const rowValues = [1, "John Doe", "john@example.com"];
+ * rowAlreadyDone(rowValues);
+ */
 function rowAlreadyDone(values) {
   const error = {
     reason: "This row has already been done. Code should not have selected that row.",
@@ -29,6 +40,34 @@ function onFormSubmission() {
   FormApp.openByUrl(sheet.getFormUrl()).deleteAllResponses();
 }
 
+/**
+ * Creates a user object from the given range data and stores it in a Google Sheets range.
+ *
+ * @param {GoogleAppsScript.Spreadsheet.Range} range - A range of cells in Google Sheets.
+ * @returns {Object} The user object with the following properties:
+ *   - timestamp {string} - The timestamp from the range data (userInfoArray[0]).
+ *   - firstName {string} - The capitalized first name from the range data (userInfoArray[1]).
+ *   - lastName {string} - The capitalized last name from the range data (userInfoArray[2]).
+ *   - password {string} - The password from the range data (userInfoArray[3]).
+ *   - phone {string} - The formatted phone number from the range data (userInfoArray[4]).
+ *   - email {string} - The lowercase email address from the range data (userInfoArray[5]).
+ *   - professor {string} - The capitalized professor name from the range data (userInfoArray[6]).
+ *   - eid {string} - The lowercase EID (Employee ID) from the range data (userInfoArray[7]).
+ *   - name {string} - The capitalized full name derived from firstName and lastName.
+ *   - username {string} - The username generated from the first and last name, with non-alphabetic characters removed.
+ *   - activation {Date} - The next business day's date based on the timestamp (userInfoArray[0]).
+ *   - record {function} - A function that returns the JSON string representation of the user object.
+ *
+ * @example
+ * // Assuming the input range contains user information in columns A to I,
+ * // and the output will be written to columns J to R.
+ * const inputRange = sheet.getRange("A2:I2");
+ * const userObject = getUserObj(inputRange);
+ * // The userObject is created and stored in columns J to R based on the information in the input range.
+ * console.log(userObject.firstName); // Output: "John"
+ * console.log(userObject.lastName); // Output: "Doe"
+ * console.log(userObject.username); // Output: "johndoe"
+ */
 function getUserObj(range) {
   const userInfoArray = range.getValues().flat();
   if (userInfoArray[8] !== "") {
@@ -53,6 +92,31 @@ function getUserObj(range) {
   return userObj;
 }
 
+/**
+ * Saves user information to a Google Calendar as an event.
+ *
+ * @param {Object} userInfoObj - The user information object.
+ * @param {string} userInfoObj.name - The full name of the user.
+ * @param {Date} userInfoObj.activation - The activation date for the user's account.
+ *
+ * @example
+ * // Example user information object:
+ * const userInformation = {
+ *   name: "John Doe",
+ *   activation: new Date("2023-07-25T12:00:00"),
+ * };
+ *
+ * // Save the user information to the calendar.
+ * saveToCalendar(userInformation);
+ *
+ * // This will create a new event on the calendar:
+ * // Title: "Lab Access: Create Account | User: John Doe"
+ * // Start: July 25, 2023 12:00:00 PM
+ * // End: July 25, 2023 12:10:00 PM (10 minutes duration)
+ * // Description: The event description generated from the "text message template" file
+ * // Color: Gray
+ * // Popup reminder: 15 minutes before the event.
+ */
 function saveToCalendar(userInfoObj) {
   const start = new Date(userInfoObj.activation);
   const end = new Date(start.getTime() + 10 * 60 * 1000);
@@ -68,6 +132,27 @@ function saveToCalendar(userInfoObj) {
     .addPopupReminder(15);
 }
 
+/**
+ * Adds a rich-text link to the user registration EID (Employee ID) on the specified sheet.
+ *
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet - The Google Sheets sheet to add the rich-text link.
+ * @param {Object} userInfoObj - The user information object returned from getUserObj function.
+ * @param {string} userInfoObj.eid - The lowercase Employee ID (EID) of the user.
+ *
+ * @example
+ * // Assuming the input range contains user information in columns A to I,
+ * // and the output will be written to columns J to R in Google Sheets.
+ * const inputRange = sheet.getRange("A2:I2");
+ * const userObject = getUserObj(inputRange);
+ * // The userObject is created and stored in columns J to R based on the information in the input range.
+ * // The user information object can now be used to add a rich-text link to the EID on the specified sheet.
+ * addRichTextUserRegistration(sheet, userObject);
+ *
+ * // This will add a rich-text link to the EID on the specified sheet:
+ * // For example, if the EID is "abcd123":
+ * // The cell in column H (last row of the sheet) will contain a hyperlink to
+ * // "https://utdirect.utexas.edu/webapps/eidlisting/eid_details?eid=abcd123".
+ */
 function addRichTextUserRegistration(sheet, userInfoObj) {
   const hyperlinkEid = SpreadsheetApp.newRichTextValue()
     .setText(userInfoObj.eid)
@@ -77,7 +162,45 @@ function addRichTextUserRegistration(sheet, userInfoObj) {
   sheet.getRange(`H${sheet.getLastRow()}`).setRichTextValue(hyperlinkEid);
 }
 
-function formatUserRegistrationSheet(sheet, userInfoObj) {
+/**
+ * Formats the user registration sheet with specified font, font size, font color,
+ * column widths, and date number format.
+ *
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet - The Google Sheets sheet to format.
+ *
+ * @example
+ * // Assuming the user registration sheet is already defined as "sheet".
+ * // Format the user registration sheet with the specified settings.
+ * formatUserRegistrationSheet(sheet);
+ */
+function formatUserRegistrationSheet(sheet) {
+  // Function implementation...
+}
+
+/**
+ * Extracts user information from the given range data and creates a basket object.
+ *
+ * @param {GoogleAppsScript.Spreadsheet.Range} range - A range of cells in Google Sheets containing user information.
+ * @returns {Object} The basket object with the following properties:
+ *   - timestamp {string} - The timestamp from the range data (values[0]).
+ *   - eid {string} - The lowercase Employee ID (EID) from the range data (values[1]).
+ *   - phone {string} - The formatted phone number from the range data (values[2]).
+ *   - email {string} - The lowercase email address from the range data (values[3]).
+ *   - firstName {string} - The capitalized first name from the range data (values[4]).
+ *   - lastName {string} - The capitalized last name from the range data (values[5]).
+ *   - status {string} - The status from the range data (values[6]).
+ *   - basket {string} - The basket information from the range data (values[7]).
+ *
+ * @example
+ * // Assuming the input range contains user information in columns A to H,
+ * // and the output will be written to columns I to P in Google Sheets.
+ * const inputRange = sheet.getRange("A2:H2");
+ * const basketObject = getBasketObj(inputRange);
+ * // The basketObject is created based on the information in the input range.
+ * console.log(basketObject.firstName); // Output: "John"
+ * console.log(basketObject.lastName); // Output: "Doe"
+ */
+function formatUserRegistrationSheet(sheet) {
   const minColumnWidths = [100, 100, 100, 100, 130, 130, 160, 100];
   sheet.getDataRange()
     .setFontFamily("Times New Roman")
@@ -95,6 +218,38 @@ function formatUserRegistrationSheet(sheet, userInfoObj) {
   });
 }
 
+/**
+ * Extracts user information from the given range data and creates a basket object with associated QR code data.
+ *
+ * @param {GoogleAppsScript.Spreadsheet.Range} range - A range of cells in Google Sheets containing user information.
+ * @returns {[Object, Object]} An array containing the basket object and the QR code data object.
+ *   - basketObj {Object} - The basket object with the following properties:
+ *     - timestamp {string} - The timestamp from the range data (values[0]).
+ *     - eid {string} - The lowercase Employee ID (EID) from the range data (values[1]).
+ *     - phone {string} - The formatted phone number from the range data (values[2]).
+ *     - email {string} - The lowercase email address from the range data (values[3]).
+ *     - firstName {string} - The capitalized first name from the range data (values[4]).
+ *     - lastName {string} - The capitalized last name from the range data (values[5]).
+ *     - status {string} - The status from the range data (values[6]).
+ *     - basket {string} - The basket information from the range data (values[7]).
+ *   - qrCodeData {Object} - The QR code data object with the following properties:
+ *     - eid {string} - The lowercase Employee ID (EID) from the basketObj.
+ *     - name {string} - The full name (first name and last name) from the basketObj.
+ *     - phone {string} - The phone number from the basketObj.
+ *     - email {string} - The email address from the basketObj.
+ *     - basket {string} - The basket information as a string from the basketObj.
+ *     - assigned {string} - The formatted timestamp (YYYY-MM-DD) from the basketObj.
+ *     - hash {function} - A function that returns the SHA-256 hash of the QR code data as a hexadecimal string.
+ *
+ * @example
+ * // Assuming the input range contains user information in columns A to H,
+ * // and the output will be written to columns I to P in Google Sheets.
+ * const inputRange = sheet.getRange("A2:H2");
+ * const [basketObject, qrCodeData] = getBasketObj(inputRange);
+ * // The basketObject and qrCodeData are created based on the information in the input range.
+ * console.log(basketObject.firstName); // Output: "John"
+ * console.log(qrCodeData.email); // Output: "john.doe@example.com"
+ */
 function getBasketObj(range) {
   const values = range.getValues().flat();
   if (values[8] !== "") {
@@ -129,7 +284,30 @@ function getBasketObj(range) {
   return [basketObj, qrCodeData];
 }
 
-
+/**
+ * Generates a QR code for the given QR code data and creates a PDF file containing the QR code image.
+ *
+ * @param {Object} qrCodeData - The QR code data object containing information for generating the QR code.
+ * @param {string} [size="255"] - The size of the QR code image (width and height) in pixels. Defaults to "255".
+ * @returns {GoogleAppsScript.Base.Blob} The QR code as a PDF file in the form of a Blob.
+ *
+ * @example
+ * // Example QR code data object:
+ * const qrCodeData = {
+ *   eid: "abcd123",
+ *   name: "John Doe",
+ *   phone: "123-456-7890",
+ *   email: "john.doe@example.com",
+ *   basket: "Basket1",
+ *   assigned: "2023-07-25",
+ *   hash: "abcdef0123456789", // The hash should be already calculated.
+ * };
+ *
+ * // Generate a QR code PDF file using the provided QR code data.
+ * const qrCodePdf = generateBasketQRCode(qrCodeData, "300");
+ *
+ * // Now you can use qrCodePdf to do further processing, such as sending it as an email attachment or saving it to Drive.
+ */
 function generateBasketQRCode(qrCodeData, size = "255") {
   const encodedData = encodeURIComponent(JSON.stringify(qrCodeData));
   const fileName = `${qrCodeData.basket.padEnd(5, " ")}${qrCodeData.assigned.padEnd(12, " ")}${qrCodeData.name}`;
@@ -145,6 +323,23 @@ function generateBasketQRCode(qrCodeData, size = "255") {
   return pdfFile.getAs(MimeType.PDF);
 }
 
+/**
+ * Adds a rich-text link to the basket registration EID (Employee ID) on the specified sheet.
+ *
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet - The Google Sheets sheet to add the rich-text link.
+ * @param {Object} userInfoObj - The user information object returned from getBasketObj function.
+ * @param {string} userInfoObj.eid - The lowercase Employee ID (EID) of the user.
+ *
+ * @example
+ * // Assuming the user information object with the basket data is already defined as "userInfo".
+ * // Add a rich-text link to the basket registration EID on the specified sheet.
+ * addRichTextBasketRegistration(sheet, userInfo);
+ *
+ * // This will add a rich-text link to the EID on the specified sheet:
+ * // For example, if the EID is "abcd123":
+ * // The cell in column B (last row of the sheet) will contain a hyperlink to
+ * // "https://utdirect.utexas.edu/webapps/eidlisting/eid_details?eid=abcd123".
+ */
 function addRichTextBasketRegistration(sheet, userInfoObj) {
   const hyperlinkEid = SpreadsheetApp.newRichTextValue()
     .setText(userInfoObj.eid)
@@ -154,7 +349,17 @@ function addRichTextBasketRegistration(sheet, userInfoObj) {
   sheet.getRange(`B${sheet.getLastRow()}`).setRichTextValue(hyperlinkEid);
 }
 
-
+/**
+ * Formats the basket assignment sheet with specified font, font size, font color,
+ * column widths, and date/number formats.
+ *
+ * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet - The Google Sheets sheet to format.
+ *
+ * @example
+ * // Assuming the basket assignment sheet is already defined as "sheet".
+ * // Format the basket assignment sheet with the specified settings.
+ * formatBasketAssignmentSheet(sheet);
+ */
 function formatBasketAssignmentSheet(sheet) {
   const minColumnWidths = [100, 100, 130, 100, 100, 150, 150];
   sheet.getDataRange()
